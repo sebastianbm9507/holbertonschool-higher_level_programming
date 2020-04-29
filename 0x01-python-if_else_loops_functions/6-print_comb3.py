e5a70497c2f0db4643d4de3ccde412a0e2e54a64
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-04-29 02:59:05 +0000

    First digit greater

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    first_digit = int(i / 10)
+    second_digit = i % 10
+    if first_digit < second_digit:
+        if i == 89:
+            print("{:d}".format(i))
+        else:
+            print("{:02d}".format(i), end=", ")
+            
