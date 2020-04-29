f57e3afc35ef5559645e29399fbb69a9851a6d2a
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-04-29 02:46:05 +0000

    Logic changed

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    if number <= 9:
-        unit = number / 10
-        print("0{}".format(number), end=", ")
+    if number == 99:
+        # 02d formats an integer (d) to a field of minimum width 2 (2) #
+        # with zero-padding on the left (leading 0):#
+        print("{:02d}".format(number))
     else:
-        if number != 99:
-            print("{}".format(number), end=", ")
-        else:
-            print(number)
+        print("{:02d}, ".format(number), end="")
