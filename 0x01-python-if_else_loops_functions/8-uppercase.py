e71cde7d3d528b5f4953006a4fe64ed970529538
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-04-29 05:00:17 +0000

    Uppercasing

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            chr = chr(ord(ch) - 32)
+        print("{}".format(ch), end="")
+    print("")
+    
