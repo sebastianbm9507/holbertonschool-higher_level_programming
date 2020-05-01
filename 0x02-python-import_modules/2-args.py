7521e1eb57b00c07e3c7ad2f7c889667fd592254
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-01 01:02:35 +0000

    Args on python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
 # going throug list ✅#
     for i in range(len(sys.argv)):
         if i == 0:
-            continue:
+            continue
         print("{:d}: {:s}".format(i, sys.argv[i]))
