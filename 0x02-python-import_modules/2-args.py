beb8a651c1cf27dd53c51488e5b4e10582c02f6a
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-01 00:58:22 +0000

    Args on python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+# printing the number of argv 🔢 #
+args_number = len(sys.argv) - 1
+# checking 0 arguments  ✅#
+if args_number == 1:
+    print("{:d} argument.".format(len(sys.argv) - 1))
+# checking 1 argument 🟢 #
+elif args_number == 2:
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+# there is more than 2 arguments 🟣 #
+else:
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+# going throug list ✅#
+for i in range(len(sys.argv)):
+    if i == 0:
+        continue;
+    print("{:d}: {:s}".format(i, sys.argv[i]))
