84809118d6db6478fa8cfadc5fd3c2f346ed8db6
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-05 20:38:34 +0000

    Element at indez

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     # Variable to store the length of list 🛃 #
     len_list = len(my_list)
     # checking if negative or greater than length list 🈂️#
-    if idx < 0 or idx > len_list:
+    if idx < 0 or idx >= len_list:
         return None
     # Returning element at idx 🆗#
-    return my_list[idx]
+    return my_list.pop(idx)
