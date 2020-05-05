d5f39a20b35bae5836077c64d8e31d61708dacbd
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-05 22:49:11 +0000

    Delete at index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    # checking cases ✅
     if idx < 0 or idx >= len(my_list):
         return my_list
+    # iterating the list 🔄
     for i in my_list:
+        # found it 🆗
         if my_list[idx] == i:
             my_list.remove(i)
     return my_list
