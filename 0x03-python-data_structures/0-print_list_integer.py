0e96aa3922bb7b0efeccad45bc75cd95972565d7
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-05 20:33:40 +0000

    Print integer

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # Variable to store the length of list 🛃 #
+    len_list = len(my_list)
+    # iterating into list 📶#
+    for i in range(len_list):
+        print("{:d}".format(my_list[i]))
+
