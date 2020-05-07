056f466c0b1bdaf397a8d8a176bc315bef337dbc
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-07 21:59:22 +0000

    Add unique number in list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # new list 
+    new_list = []
+    for element in my_list:
+        # checking element not exist in list 🔄
+        if element not in new_list:
+            new_list.append(element)
+    result = 0
+    # adding each element in list into result 🔴
+    for item in new_list:
+        result += item
+    return result
