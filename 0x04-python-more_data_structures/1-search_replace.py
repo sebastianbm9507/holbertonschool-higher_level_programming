0731311124346dbaefee33e5425aa03fe24fac83
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-07 21:18:07 +0000

    Replace element of list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,10 @@
 def search_replace(my_list, search, replace):
     # Copy of list ✅
     copy_list = my_list.copy()
+    idx = 0
     for i in copy_list:
         if i == search:
             # replace the element 🔁
-            copy_list[i] = replace
+            copy_list[idx] = replace
+        idx += 1
     return copy_list
