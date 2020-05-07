79faf02f261bcda3c20acee91243f6ace0591e77
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-07 23:36:20 +0000

    Fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -4,3 +4,8 @@ def number_keys(a_dictionary):
     for ky, valu in a_dictionary.items():
         counter += 1
     return counter
+"""
+    return len of dictionary also works
+    example: return (len(a_dictionary))
+
+"""
