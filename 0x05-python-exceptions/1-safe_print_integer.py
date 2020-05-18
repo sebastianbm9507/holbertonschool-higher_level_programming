6d9c47254313c7e54c3b6ede12ac0aaa52ecdc7e
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-18 21:12:41 +0000

    Print integer safe

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
-        print(err)
+    except:
         return False
