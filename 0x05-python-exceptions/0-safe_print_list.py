8972e0104912c453616b76900cac9b87d304af61
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-18 20:36:04 +0000

    Print safe list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,3 +10,12 @@ def safe_print_list(my_list=[], x=0):
         return counter
     except IndexError as error:
         print(erro)
+
+""" ✅
+ Another solution :
+ try:
+        for item in range(0, x):
+            print("{}".format(my_list[item]), end="")
+            num += 1
+    except IndexError:
+"""
