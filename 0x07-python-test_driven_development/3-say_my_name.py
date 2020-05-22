cde6ca48e74120373bb8373e5e0955bf10a0390d
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-22 21:47:59 +0000

    Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines the say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that says a name
+
+    Args:
+        first_name (string): first_name.
+        last_name (string): last_name.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
