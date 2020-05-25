8b8e7facd9b20f8de44ce0a7a6b6cdc173231181
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-05-25 23:55:35 +0000

    Rectangle setter and getter property

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -63,3 +63,6 @@ class Rectangle:
         """
         if type(value) != int:
             raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
