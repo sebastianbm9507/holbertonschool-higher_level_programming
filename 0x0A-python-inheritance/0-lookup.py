7b24ff86e967d987d9679dbc0d347a4ae072d6b7
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-06-02 23:08:49 +0000

    0-lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Module that creates lookup function
+"""
+
+
+def lookup(obj):
+    """ Lookup Function
+
+    Arguments:
+        obj parameter
+
+    Returns:
+        dir(ob) -> Attributes of the obj
+    """
+    return dir(obj)
