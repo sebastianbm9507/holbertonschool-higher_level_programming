bb8202517eb8d0ea5a73994a92e53edc706c64cd
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-06-03 23:13:49 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Module that creates a read_file function
+"""
+
+
+def read_file(filename=""):
+    """Read a file content
+
+    Args:
+        filename (str, optional): file to be read. Defaults to "".
+    """
+    with open(filename, 'r', encoding='UTF8') as file:
+        data = file.read()
+    print(data)
