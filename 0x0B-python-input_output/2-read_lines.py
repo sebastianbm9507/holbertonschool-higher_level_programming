8b4e345f165d4301fdb5254b73da5f669cb4b7a9
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-06-03 23:23:02 +0000

    Read till n line

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module that creates a read_lines function
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """[summary]
+
+    Args:
+        filename (str, optional): [description]. Defaults to "".
+        nb_lines (int, optional): [description]. Defaults to 0.
+    """
+    with open(filename, 'r') as file:
+        n_lines = 0
+        for lines in file:
+            n_lines += 1
+
+        file.seek(0)
+        if nb_lines <= 0 or nb_lines >= n_lines:
+            file_data = file.read()
+            print(file_data, end='')
+        else:
+            for line in range(nb_lines):
+                file_data = file.readline()
+                print(file_data, end='')
+        print()
