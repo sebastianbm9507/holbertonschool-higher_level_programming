59d47aeb5470f10bee57c705fdfc459aa28161b9
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-06-03 23:41:09 +0000

    Load json string

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -7,4 +7,12 @@ import json
 
 
 def from_json_string(my_str):
+    """Function that load a string
+
+    Args:
+        my_str (str): parameter
+
+    Returns:
+       JSON: json string
+    """
     return (json.loads(my_str))
