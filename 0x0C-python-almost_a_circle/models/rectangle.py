8d6a95c76baf7d99a7b1d0811df82e2930add841
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-06-09 20:22:58 +0000

    Rectangle module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,7 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    """Rectangle Class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Init method for class Base
