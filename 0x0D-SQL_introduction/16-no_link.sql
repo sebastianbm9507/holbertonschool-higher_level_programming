4f3a8bdc1816e2c7ab18964b0c256bccb23db4fe
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-13 22:19:06 +0000

    Hot fix uppercase

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- List All records table
 --  Script that lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, name from second_table where name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
