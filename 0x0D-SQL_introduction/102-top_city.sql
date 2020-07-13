b8a241f3a160a9abf88a24a7354dc9460436374f
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-13 23:01:29 +0000

    Top 3 temperatures

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Top temeperatures
 -- Show top 3 temperatures of database
-SELECT city, AVG(temperatures) AS avg_temp FROM temperatures WHERE month BETWEEN 8 AND 9 GROUP BY avg_temp ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month BETWEEN 8 AND 9 GROUP BY avg_temp ORDER BY avg_temp DESC LIMIT 3;
