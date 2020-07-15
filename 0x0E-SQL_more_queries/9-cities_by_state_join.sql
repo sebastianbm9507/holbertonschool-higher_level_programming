d4403aa0e498a5e6e643216237eac51884a2a5c2
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-07-15 03:18:57 +0000

    Show cities and states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Select cities 
 -- lists all cities contained in the database hbtn_0d_usa.
-select cities.id, cities.name, states.name from cities, states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities, states ORDER BY cities.id ASC;
