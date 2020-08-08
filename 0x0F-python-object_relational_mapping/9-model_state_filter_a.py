9aef494a54df0cc9eba8babf33e32732fa6c81b1
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-08 19:24:32 +0000

    Query containing A letter

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" ℹ️
+    prints the first State object
+    from the database hbtn_0e_6_usa
+"""
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+
+    # Parameter variables 🎟
+    user = argv[1]
+    passw = argv[2]
+    database = argv[3]
+
+    # ᐁ Create the engine
+    engine = create_engine(
+                    'mysql+mysqldb://{}:{}@localhost/{}'
+                    .format(user, passw, database), pool_pre_ping=True
+                    )
+    # create the session instant and bind the engine
+    Session = sessionmaker(bind=engine)
+    # Create the session
+    session = Session()
+
+    # Query docs
+    # ℹ️ https://docs.sqlalchemy.org/en/13/orm/query.html
+    query = session.query(State).order_by(State.id).all()
+
+    for value in query:
+        if 'a' in value:
+            print("{}: {}".format(value.id, value.name))
+    # Close session
+    session.close()
