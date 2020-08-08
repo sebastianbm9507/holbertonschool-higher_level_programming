7abc1cebee8c6a18550b1cec8b77b847a650da33
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-08 20:47:25 +0000

    Update row

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" ℹ️
+    prints the State object with the name passed '
+    as argument from the database hbtn_0e_6_usa
+"""
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
+    from model_state import Base, State
+
+    # Parameter variables 🎟
+    user = argv[1]
+    passw = argv[2]
+    database = argv[3]
+
+    # ᐁ Create the engine
+    engine = create_engine(
+                    'mysql+mysqldb://{}:{}@localhost/{}'
+                    .format(user, passw, database), pool_pre_ping=True
+                    )
+    # create the session instant and bind the engine
+    Session = sessionmaker(bind=engine)
+    # Create the session
+    session = Session()
+    # Query
+    session.query(State).filter(State.id == 2)
+    .update({State.name: 'New Mexico'})
+    session.commit()
