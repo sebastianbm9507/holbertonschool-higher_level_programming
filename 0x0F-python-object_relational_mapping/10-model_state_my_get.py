64f93b92ed31566351a425d9038b8773111ed336
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-08 19:39:48 +0000

    Print state by argument

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     # Query docs
     # ℹ️ https://docs.sqlalchemy.org/en/13/orm/query.html
-    query = session.query(State).filter(State.name == word_to_match).all()
+    query = session.query(State).filter(State.name == word_to_match).first()
 
     if query:
         print("{}".format(query.id))
