a6ed6b9b21113b42ee1deff7547724d5d2624d59
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-08 14:51:24 +0000

    Fix query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("""SELECT *
                 FROM states
-                WHERE name='{:s}' ORDER BY states.id""".format(word))
+                WHERE name LIKE '{}' ORDER BY id ASC""".format(word))
     rows = cur.fetchall()
     for row in rows:
         print(row)
