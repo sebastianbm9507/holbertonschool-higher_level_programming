59c8ec045cfef63a9b79af3008d0fed3949b0553
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-19 00:41:18 +0000

    Fix shebang

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,10 @@
-#!#usr/bin/python3
+#!usr/bin/python3
 """ Python module  """
 
 
 def find_peak(list_of_integers):
     """ Function to find a peak """
-    if list_of_integers == []:
+    if not len(list_of_integers):
         return None
     list_of_integers.sort()
     return list_of_integers[-1]
