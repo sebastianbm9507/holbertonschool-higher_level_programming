de05eaf773097310b875718650da9622fe03d594
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-19 00:45:58 +0000

    Fix

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 def find_peak(list_of_integers):
     """ Function to find a peak """
-    if not len(list_of_integers):
+    if list_of_integers == []:
         return None
     list_of_integers.sort()
     return list_of_integers[-1]
