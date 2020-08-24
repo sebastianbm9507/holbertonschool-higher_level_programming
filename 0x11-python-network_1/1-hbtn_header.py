de48d8216036b12f345884691f7cf6f58c1cb5c1
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-24 22:34:31 +0000

    Headers of request

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Read header of a request """
 
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
-import urllib.request
-import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    print(headers['X-Request-Id'])
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        print(headers['X-Request-Id'])
