a2607b58c10f9f0a8fb5481e5247f626cd50f059
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 00:11:48 +0000

    Headers in requests library

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Sends a request to the URL and displays the value
+    of the variable X-Request-Id in the response header
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    result_request = requests.get(url)
+    header_id = result_request.headers['X-Request-Id']
+    print(header_id)
