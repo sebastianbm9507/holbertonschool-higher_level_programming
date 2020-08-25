3be96a97a6a1d738006c5c509e0a7f708134a6ee
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 01:34:36 +0000

    Api request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -12,13 +12,13 @@ if __name__ == "__main__":
         values = {'q': sys.argv[1]}
     else:
         values = {'q': ""}
-    # POST REQUEST
-    result = requests.post('http://0.0.0.0:5000/search_user', data=values)
-    if result.headers.get('content-type') == 'application/json':
-        if not result.json():
+    # Post request
+    response = requests.post('http://0.0.0.0:5000/search_user', data=values)
+    if response.headers.get('content-type') == 'application/json':
+        if not response.json():
             print("No result")
         else:
             print("[{}] {}\
-".format(result.json()['id'], result.json()['name']))
+".format(response.json()['id'], response.json()['name']))
     else:
         print("Not a valid JSON")
