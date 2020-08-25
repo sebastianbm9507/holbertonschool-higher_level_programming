77ea59ff611025ab63e8ca55361d945e8836e153
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 00:36:50 +0000

    Email with requests

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email 
+    as a parameter, and finally displays the body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    result = requests.post('https://httpbin.org/post', {'email': email})
+    print(result.text)
