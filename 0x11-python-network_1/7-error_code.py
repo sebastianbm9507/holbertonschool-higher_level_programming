36b3da0e3eb8cabb090687fbd853e490bdf3c2ac
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 01:14:03 +0000

    Error code with requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" sends a request to the URL and
-    displays the body of the response.
-"""
+
 
 if __name__ == '__main__':
     import requests
     import sys
 
     url = sys.argv[1]
-    try:
-        result = requests.get(url)
-        print(result.text)
-    except Exception as error:
-        print('Error code: {}'.format(error.status_code))
+    result = requests.get(url)
+    if result.status_code >= 400:
+        print('Error code: {}'.format(result.status_code))
+    else:
+        print(result)
