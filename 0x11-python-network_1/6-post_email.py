f53ef24f5441cb7fbe199b849e26da16bbf3ade2
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 00:37:19 +0000

    Email with requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" sends a POST request to the passed URL with the email 
+""" sends a POST request to the passed URL with the email
     as a parameter, and finally displays the body of the response.
 """
 
