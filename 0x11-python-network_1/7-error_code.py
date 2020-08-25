541c326222d8282e1ab4c896fceecec501e8bfb7
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-25 00:54:09 +0000

    Error code with requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" sends a request to the URL and
+    displays the body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    try:
+        result = requests.get(url)
+        print('Error code: {}', result)
+    except Exception as error:
+        print('Error code: {}'.format(error))
