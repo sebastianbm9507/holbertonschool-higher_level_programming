3e0bd387f7cab3a1933591701a40da15fbc3f445
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-15 22:29:00 +0000

    Fix slice

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (!process.argv[2] || process.argv.length === 3) {
   console.log(0);
 } else {
-  const array = process.argv.slice(2, process.argv.length).sort();
+  const array = process.argv.slice(2).sort();
   // using spread operator 🍒
   // let [a, b, ...array] = process.argv.sort();
   console.log(array[array.length - 2]);
