5a35ed4df02dfd28978ff65563e12ce2f7fa2424
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-15 22:32:27 +0000

    Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (!process.argv[2] || process.argv.length === 3) {
   console.log(0);
 } else {
-  const array = process.argv.slice(2).sort();
+  const array = process.argv.slice(2).sort((a, b) => a - b);
   // using spread operator 🍒
   // let [a, b, ...array] = process.argv.sort();
   console.log(array[array.length - 2]);
