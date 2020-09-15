c5dda83746a9cdff4cce31888d8fe3cea4620ee6
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-15 22:19:32 +0000

    Try with slice

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,8 @@
 if (!process.argv[2] || process.argv.length === 3) {
   console.log(0);
 } else {
-  const [a, b, ...array] = process.argv.sort();
+  const array = process.argv.slice(2, process.argv.length).sort();
+  // using spread operator 🍒
+  // let [a, b, ...array] = process.argv.sort();
   console.log(array[array.length - 2]);
 }
-
