4ba75d3f9ae47567b6f7460fe37811e0ad417861
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-15 22:05:02 +0000

    Find the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (!process.argv[2] || process.argv.length === 3) {
+  console.log(0);
+} else {
+  const [a, b, ...array] = process.argv.sort();
+  console.log(array[array.length - 2]);
+}
+
