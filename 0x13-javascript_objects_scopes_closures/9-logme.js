30c5e551f12920e1dcdfb5a638a5b58b5ea99b40
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-21 23:06:46 +0000

    Change var to let and implement string literals

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-var counter = 0;
+let counter = 0;
 exports.logMe = function (item) {
-  console.log(counter + ': ' + item);
+  console.log(`${counter}: ${item}`);
   counter++;
 };
