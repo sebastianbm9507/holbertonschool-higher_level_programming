0b98b77a0ecb773d537cc0be95909e93b21535a1
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-22 17:12:37 +0000

    Add readme and task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!usr/bin/node
+
+const fs = require('fs');
+
+const readAFile = path => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.error('An error ocurred', err);
+    } else {
+      console.log(data);
+    }
+  });
+};
+readAFile(process.argv[2]);
