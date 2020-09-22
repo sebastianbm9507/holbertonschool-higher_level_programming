ef38301fe066e12c62b053b22bb2e3776359ddce
untalsebastianb <juan.bueno01@usc.edu.co>
2020-09-22 16:28:13 -0500

    Delete global variable

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,18 @@ const request = require('request');
 
 const URL = 'https://swapi-api.hbtn.io/api/films/';
 const ID = 18;
-var ocurrences = 0;
-request(URL, (req, res, body) => {
-  const data = JSON.parse(body).results;
-  data.forEach(item => {
-    const characters = item.characters;
-    characters.filter(item => {
-      if (item.includes(ID)) ocurrences++;
-    }); // end filter 🔚
-  }); // end for each 🔚
-  console.log(ocurrences);
+request(URL, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    let ocurrences = 0;
+    const data = JSON.parse(body).results;
+    data.forEach(item => {
+      const characters = item.characters;
+      characters.filter(item => {
+        if (item.includes(ID)) ocurrences++;
+      }); // end filter 🔚
+    }); // end for each 🔚
+    console.log(ocurrences);
+  } // else end 🔚
 }); // end request 🔚
