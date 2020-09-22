39b412c135fcab28dfdb5b2c29d7643ad02b9aa8
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-22 01:37:08 +0000

    Fixing the way to append to file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,21 +1,22 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const readData = (ruta) => {
-  try {
-    return fs.readFileSync(ruta, 'utf-8');
-  } catch (err) {
-    console.log(err);
-  }
-};
+let dataFile1 = '';
+let dataFile2 = '';
 //  Gathering data 📀
-const dataFile1 = readData(process.argv[2]);
-const dataFile2 = readData(process.argv[3]);
-
+try {
+  dataFile1 = fs.readFileSync(process.argv[2], 'utf8');
+  dataFile2 = fs.readFileSync(process.argv[3], 'utf-8');
+} catch (err) {
+  console.error(err);
+}
 // Writting file 📂
-fs.writeFile(process.argv[4], dataFile1 + dataFile2, (err) => {
-  // Catch error with first error callback technique ❗️
-  if (err) {
-    console.log('An error has occurred');
-  }
-});
+const writeToFile = (data) => {
+  fs.appendFile(process.argv[4], data + '\n', (err) => {
+    if (err) {
+      return console.error(err);
+    }
+  });
+};
+writeToFile(dataFile1);
+writeToFile(dataFile2);
