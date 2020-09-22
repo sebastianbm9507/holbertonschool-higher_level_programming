6f88bbb25ba74d604fbeda295e534d6674fa8f1e
Juan Sebastian <juan.bueno01@usc.edu.co>
2020-09-22 17:20:16 +0000

    Change stoudt

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,11 +4,7 @@ const fs = require('fs');
 
 const readAFile = path => {
   fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
+    console.log(err || data);
   });
 };
 readAFile(process.argv[2]);
